18ae5d0084525522fe2d803caee59b97c4d35d3b
Ljubomir Malic <ljubomir.malic@ebs.msft>
2019-07-13 13:24:57 +0200

    Added project dependencies

---
+++ vendor/github.com/sn-srdjan/cx-tracker-cli/src/cli/version.go
@@ -0,0 +1,44 @@
+package cli
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/spf13/cobra"
+)
+
+func versionCmd() *cobra.Command {
+	versionCmd := &cobra.Command{
+		Use:          "version",
+		Short:        "List the current version of CX Tracker components",
+		Args:         cobra.NoArgs,
+		SilenceUsage: true,
+		RunE: func(c *cobra.Command, _ []string) error {
+			var ver = struct {
+				Cli string `json:"cli"`
+			}{
+				Version,
+			}
+
+			jsonOutput, err := c.Flags().GetBool("json")
+			if err != nil {
+				return err
+			}
+			if jsonOutput {
+				return printJSON(ver)
+			}
+
+			v := reflect.ValueOf(ver)
+			t := reflect.TypeOf(ver)
+			for i := 0; i < v.NumField(); i++ {
+				fmt.Printf("%s:%v\n", t.Field(i).Tag.Get("json"), v.Field(i).Interface())
+			}
+
+			return nil
+		},
+	}
+
+	versionCmd.Flags().BoolP("json", "j", false, "Returns the results in JSON format")
+
+	return versionCmd
+}
